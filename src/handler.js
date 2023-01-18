aa4efd344f9080a8ad9eee2f80de8d8e02ca8082
sushantkaranjikar <117449742+sushantkaranjikar@users.noreply.github.com>
2023-01-18 15:17:01 +0530

    Update handler.js

--- src/handler.js
+++ src/handler.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Welcome to TechnoCirrus!!!'),
+        body: JSON.stringify('Welcome to TechnoCirrus 1!!!'),
     };
     return response;
 };
